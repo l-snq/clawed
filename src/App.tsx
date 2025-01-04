1b2857b943f548782b09c1634f9cb0722ae1ecb9
ttjdesktop <tastytothesupreme@gmail.com>
2025-01-03 21:50:20 -0700

    added ping function

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+	const [pingMsg, setPingMsg ] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
 
+	async function ping() {
+		setPingMsg(await invoke("ping", {}));
+	}
+
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <h1>clawed. </h1>
+			<div>
+				{pingMsg ? "yo" : "no"}
+			</div>
 
       <form
         className="row"
