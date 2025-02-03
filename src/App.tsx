c6a4c6b40137db9e7db97cf03f60917425319f5d
lsnq <tastytothesupreme@gmail.com>
2025-02-02 23:05:01 -0700

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,7 @@ import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-	const [something, setSomething] = useState("");
 	const [scrape, setScrape] = useState([]);
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
 
 	async function scrapeReq() {
 		setScrape(await invoke("scrapeDataCommand", { scrape }));
@@ -23,7 +16,6 @@ function App() {
       <h1>clawed. </h1>
 			<div>
 				<button onClick={(e) => {
-					setSomething(something)
 					setScrape(scrape)
 					scrapeReq();
 				}}> click me</button>
